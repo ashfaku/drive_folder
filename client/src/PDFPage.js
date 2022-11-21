13de955c34668a8c0627dcbcafa4d5ff323625e9
Ashfak Uddin <ashmanpapet@gmail.com>
2022-11-21 13:32:25 -0500

    Idk

--- client/src/PDFPage.js
+++ client/src/PDFPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { usePdf } from '@mikecousins/react-pdf';
+import { usePdf } from 'react-pdf';
 
 const PDFPage = (props) => {
   const [page, setPage] = useState(1);
