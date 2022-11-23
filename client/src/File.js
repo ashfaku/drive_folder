3adca0ca1864cff72460b441edf968d18c9bb988
Ashfak Uddin <ripple@Ashfaks-Air.home>
2022-11-23 00:43:14 -0500

    ?

--- client/src/File.js
+++ client/src/File.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { useNavigate, createSearchParams } from "react-router-dom";
 const File = (props) => {
     var tab = props.tabbing;
     const filePath =  "./" + props.path;
