2238b3d8551cea716c7ac2a071b6a74c09f9b0d0
Ashfak Uddin <ripple@Ashfaks-Air.home>
2022-12-02 19:23:55 -0500

    idk

--- server/index.js
+++ server/index.js
@@ -4,17 +4,17 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 5001;
 //console.log(dirTree("./../client/public/PDFs"));
-const trees = dirTree('./../client/public/pdf');
+const trees = dirTree('./../client/public/PDFs');
 app.get('/express_backend', (req, res) => { 
     res.send({ files: dirTree("./../client/public/pdf") });
 });
-/*
+
 fs.writeFile('json.json', JSON.stringify(trees), function (err) {
   if (err) throw err;
   console.log('Saved!');
-});*/
-/*
-const makeNew = (tree) =>
+});
+
+/*const makeNew = (tree) =>
 {
  // console.log(tree.path);
 //  console.log(String(tree.path).replaceAll(' ', '_'));
@@ -42,8 +42,8 @@ const makeNew = (tree) =>
     }
   }
 }
-makeNew(trees);
-*/
+makeNew(trees);*/
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
